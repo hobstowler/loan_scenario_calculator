e00a5d87a5d9f7fa5079176d6c53a877d05fbb86
Hobs Towler <towlerj@oregonstate.edu>
2021-12-03 00:27:33 -0500

    messing with tkinter

--- app.py
+++ app.py
@@ -68,6 +68,7 @@ root = Tk()
 root.title("test")
 
 mainframe = ttk.Frame(root)
+mainframe.grid(column=0, row=0)
 
 left_pane = ttk.Frame(mainframe)
 left_pane.grid(column=0, row=0)
@@ -76,14 +77,19 @@ middle_pane.grid(column=1, row=0)
 right_pane = ttk.Frame(mainframe)
 right_pane.grid(column=2, row=0)
 
-nav_menu = ttk.Frame(left_pane, width=300, height=50, padding=6)
-nav_menu.grid(column=0, row=0)
-left_drawer = ttk.Frame(left_pane, borderwidth=2, relief='ridge', width=300, height=500, padding=6)
+nav_menu = ttk.Frame(left_pane, width=300, height=50, padding=8)
+nav_menu.grid(column=0, row=0, sticky=(N,W,S,E), pady=4, padx=2)
+left_drawer = ttk.Frame(left_pane, borderwidth=2, relief='sunken', width=300, height=500, padding=6)
 left_drawer.grid(column=0, row=1)
 
-info_label = ttk.Frame(middle_pane, borderwidth=2, relief='ridge', width=300, height=50, padding=6).grid(column=1, row=0)
+detail_label = ttk.Frame(middle_pane, borderwidth=2, relief='ridge', width=300, height=50, padding=6)
+detail_label.grid(column=0, row=0)
+detail_panel = ttk.Frame(middle_pane, width=300, height=500, padding=6)
+detail_panel.grid(column=0, row=1)
 
-stat_panel = ttk.Frame(right_pane, borderwidth=2, relief='ridge', width=500, height=550, padding=6).grid(column=2, row=0, rowspan=2)
+stat_panel = ttk.Frame(right_pane, borderwidth=2, relief='ridge', width=500, height=550, padding=6)
+stat_panel.grid(column=0, row=0, rowspan=2)
+
 
 scenario_button = ttk.Button(nav_menu, text="Scenarios", width=10).grid(column=0, row=0)
 job_button = ttk.Button(nav_menu, text="Jobs", width=10).grid(column=2, row=0)
