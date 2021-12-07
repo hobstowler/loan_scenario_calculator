35fc9b4a5f60690550dc702313e0a49a001ecbcc
Hobs Towler <towlerj@oregonstate.edu>
2021-12-07 17:19:12 -0500

    implemented Forms

--- Forms.py
+++ Forms.py
@@ -5,11 +5,38 @@
 class DetailForm:
     def __init__(self):
         self._forms = []
+        self.create_forms()
 
-        self._forms.append([]) # Scenarios Form
-        self._forms.append([]) # Jobs Form
-        self._forms.append([]) # Loans Form
-        self._forms.append([]) # Expenses Form
+    def create_forms(self):
+        # Scenarios Form
+        self._forms.append(["Scenario Editor",
+                            [["Label", "Name", "", 1], ["Entry", "", "", 2]],
+                            [["Label", "Description", "", 1], ["Entry", "", "", 2]],
+                            [["", "", "", 3]]
+                            ])
+        # Jobs Form
+        self._forms.append(["Job Editor",
+                            [["Label", "Title", "", 1], ["Entry", "", "", 2]],
+                            [["Label", "Company", "", 1], ["Entry", "", "", 2]],
+                            [["", "", "", 3]],
+                            [["Label", "Salary", "", 1], ["Entry", "", "", 2]]
+                            ])
+        # Loans Form
+        self._forms.append(["Loan Editor",
+                            [["Label", "Name", "", 1], ["Entry", "", "", 2]],
+                            [["", "", "", 3]],
+                            [["Label", "Loan Amount", "", 1], ["Entry", "", "", 2]],
+                            [["Label", "Down Payment", "", 1], ["Entry", "", "", 1],
+                             ["CheckButton", "Percentage", "", 1]],
+                            [["Label", "Rate", "", 1], ["Entry", "", "", 2]],
+                            [["", "", "", 3]]
+                            ])
+        # Expenses Form
+        self._forms.append(["Expense Editor",
+                            [["Label", "Name", "", 1], ["Entry", "", "", 2]],
+                            [["Label", "Description", "", 1], ["Entry", "", "", 2]],
+                            [["", "", "", 3]]
+                            ])
 
     def get_form(self, context) -> list:
         return self._forms[context]
--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import tkinter as tk
 from main import *
 from income import *
 from dataload import *
+from Forms import *
 
 
 def open_scenario():
@@ -38,58 +39,77 @@ class LoanDetail(Context):
     pass
 
 
-class NavMenu:
-    def __init__(self, parent: tk.Frame):
-        nav_menu = tk.Frame(parent, name="nav_menu", width=300, height=30)
-        nav_menu.grid(column=0, row=0, sticky=N+W+S+E, pady=(0, 0), padx=0)
+class LeftPanel:
+    def __init__(self, parent: tk.Tk):
+        self.frame = tk.Frame(parent)
+        self.frame.grid(column=0, row=0)
 
-        self._scenario_text = "Pick a Scenario."
-        self._job_text = "Pick a Job."
-        self._loan_text = "Pick a Loan."
-        self._expense_text = "Pick an Expense."
-        self._context_text = [self._scenario_text, self._job_text, self._loan_text, self._expense_text]
+        # nav menu variables
+        self._nav_label = None
         self._context = 2
+        self._context_text = [
+            "Pick a Scenario.",
+            "Pick a Job.",
+            "Pick a Loan.",
+            "Pick an Expense."
+        ]
 
+        # drawer variables
+
+        # bottom menu variables
+        self._del_button = None
+
+        # instantiate major components
+        self._nav_menu = self.create_nav_menu()
+        self._drawer = self.create_drawer()
+        self._bottom_menu = self.create_bottom_menu()
+
+    def create_nav_menu(self):
+        nav_menu = tk.Frame(self.frame, name="nav_menu", width=300, height=30)
+        nav_menu.grid(column=0, row=0, sticky=N + W + S + E, pady=(0, 0), padx=0)
         for i in range(4):
             nav_menu.columnconfigure(i, weight=1)
 
-        tk.Button(nav_menu, name="scenario_button", text="Scenarios", width=8, command=lambda: self.set_context(0))\
-            .grid(column=0, row=0, sticky=W+E)
-        tk.Button(nav_menu, name="job_button", text="Jobs", width=8, command=lambda: self.set_context(1))\
-            .grid(column=1, row=0, sticky=W+E)
-        tk.Button(nav_menu, name="loan_button", text="Loans", width=8, command=lambda: self.set_context(2))\
-            .grid(column=2, row=0, sticky=W+E)
-        tk.Button(nav_menu, name="expense_button", text="Expenses", width=8, command=lambda: self.set_context(3))\
-            .grid(column=3, row=0, sticky=W+E)
-
-        self._label = tk.Label(nav_menu, name="nav_label", text=self._context_text[0])
-        self._label.grid(column=0, row=1, columnspan=4)
-
-    def set_context(self, context: int):
-        self._label['text'] = self._context_text[context]
-        self._context = context
-
-    def get_context(self) -> int:
-        return self._context
+        tk.Button(nav_menu, name="scenario_button", text="Scenarios", width=8, command=lambda: self.set_context(0)) \
+            .grid(column=0, row=0, sticky=W + E)
+        tk.Button(nav_menu, name="job_button", text="Jobs", width=8, command=lambda: self.set_context(1)) \
+            .grid(column=1, row=0, sticky=W + E)
+        tk.Button(nav_menu, name="loan_button", text="Loans", width=8, command=lambda: self.set_context(2)) \
+            .grid(column=2, row=0, sticky=W + E)
+        tk.Button(nav_menu, name="expense_button", text="Expenses", width=8, command=lambda: self.set_context(3)) \
+            .grid(column=3, row=0, sticky=W + E)
 
 
-class BottomMenu:
-    def __init__(self, parent: tk.Frame) -> None:
-        frame = tk.Frame(parent, name="left_bottom_menu", width=300, height=50)
+        self._nav_label = tk.Label(nav_menu, name="nav_label", text=self._context_text[self._context])
+        self._nav_label.grid(column=0, row=1, columnspan=4)
+
+        return nav_menu
+
+    def create_drawer(self):
+        left_drawer = tk.Frame(self.frame, borderwidth=2, relief='sunken', width=300, height=500)
+        left_drawer.grid(column=0, row=1, sticky=N + W + S + E)
+        left_drawer.grid_propagate(False)
+
+        return left_drawer
+
+    def create_bottom_menu(self):
+        frame = tk.Frame(self.frame, name="left_bottom_menu", width=300, height=50)
         frame.grid(column=0, row=2, sticky=N + W + S + E)
 
-        frame.columnconfigure(0, weight=1)
-        frame.columnconfigure(1, weight=1)
-        frame.columnconfigure(2, weight=1)
+        for i in range(3):
+            frame.columnconfigure(i, weight=1)
 
         # Buttons for the bottom menu. Edit, New, and Delete.
         tk.Button(frame, name="edit_item", text="Edit...", command=edit_item) \
-            .grid(column=0, row=0, sticky=W + E)
-        tk.Button(frame, name="new_item", text="New...", command=lambda: mid_panel.populate(left_panel.nav().get_context())) \
-            .grid(column=1, row=0, sticky=W + E)
+            .grid(column=0, row=0, sticky=W+E)
+        tk.Button(frame, name="new_item", text="New...",
+                  command=lambda: mid_panel.populate(self.get_context())) \
+            .grid(column=1, row=0, sticky=W+E)
         self._del_button = tk.Button(frame, name="delete_item", text="Delete...", state="disabled", command=delete_item)
-        self._del_button.grid(column=2, row=0, sticky=W + E)
+        self._del_button.grid(column=2, row=0, sticky=W+E)
         tk.Checkbutton(frame, name="delete_check", command=lambda: self.toggle_delete()).grid(column=3, row=0)
+
+        return frame
 
     def toggle_delete(self):
         if self._del_button['state'] == "disabled":
@@ -97,25 +117,17 @@ class BottomMenu:
         else:
             self._del_button['state'] = "disabled"
 
+    def set_context(self, context: int):
+        self._nav_label['text'] = self._context_text[context]
+        self._context = context
 
-class LeftPanel:
-    def __init__(self, parent: tk.Tk):
-        panel = tk.Frame(parent)
-        self.frame = panel
-        panel.grid(column=0, row=0)
-        self._nav_menu = NavMenu(panel)
-        _create_left_drawer(panel)
-        self._bottom_menu = BottomMenu(panel)
-
-    def nav(self) -> NavMenu:
-        return self._nav_menu
-
-    def bottom(self) -> BottomMenu:
-        return self._bottom_menu
+    def get_context(self) -> int:
+        return self._context
 
 
 class MidPanel:
     def __init__(self, parent: tk.Tk):
+        self._forms = DetailForm()
         panel = tk.Frame(parent)
         self.frame = panel
         panel.grid(column=1, row=0, sticky=N+S)
@@ -130,35 +142,27 @@ class MidPanel:
         self.detail_panel.grid(column=0, row=1, sticky=N+E+S+W)
         self.detail_panel.grid_propagate(False)
 
-    # TODO make this form-based
     def populate(self, context: int):
         for c in self.detail_panel.winfo_children():
             c.destroy()
-        if context == 0:
-            return
-        if context == 1:
-            return
-        if context == 2:
-            self.pop_loan()
-        if context == 3:
-            return
-
-    def pop_loan(self):
         panel = self.detail_panel
-        tk.Label(panel, text="Name").grid(column=0, row=0)
-        tk.Entry(panel, width=20).grid(column=1, row=0)
+        form = self._forms.get_form(context)
+        self._label.set(form[0])
+        for i in range(1, len(form)):
+            for j in range(0, len(form[i])):
+                type = form[i][j][0]
+                text = form[i][j][1]
+                name = form[i][j][2]
+                col_span = form[i][j][3]
+                if type == "Label":
+                    tk.Label(panel, text=text).grid(row=i, column=j, columnspan=col_span)
+                if type == "Entry":
+                    tk.Entry(panel, text=text).grid(row=i, column=j, columnspan=col_span, sticky=W+E)
+                if type == "CheckButton":
+                    tk.Checkbutton(panel, text=text).grid(row=i, column=j, columnspan=col_span)
+                if type == "":
+                    tk.Frame(panel, height=10).grid(row=i, column=j, columnspan=col_span)
 
-        tk.Label(panel, text="Loan Amount").grid(column=0, row=1)
-        loan_amount = StringVar()
-        loan_amount.set("test")
-        tk.Entry(panel, width=20, textvariable=loan_amount).grid(column=1, row=1)
-
-        tk.Label(panel, text="Down Payment").grid(column=0, row=2)
-        tk.Entry(panel, width=20).grid(column=1, row=2)
-        tk.Checkbutton(panel, name="down_percent", text="Percentage").grid(column=2,row=2)
-
-        tk.Label(panel, text="Rate").grid(column=0, row=3)
-        tk.Entry(panel, width=20).grid(column=1, row=3)
         self.show_bottom_menu()
 
     def show_bottom_menu(self):
@@ -173,7 +177,7 @@ class MidPanel:
             if i % 2 == 0:
                 #add padding frames between the two buttons
                 tk.Frame(bMenu).grid(column=i, row=0)
-        tk.Button(bMenu, text="Cancel", command=lambda: self.clear()).grid(column=1, row=0, sticky=W + E)
+        tk.Button(bMenu, text="Close", command=lambda: self.clear()).grid(column=1, row=0, sticky=W + E)
         tk.Button(bMenu, text="Save").grid(column=3, row=0, sticky=W + E)
 
     def hide_bottom_menu(self):
@@ -195,27 +199,16 @@ class MidPanel:
         self.hide_bottom_menu()
 
 
-
 class RightPanel:
     def __init__(self, parent: tk.Tk):
         panel = tk.Frame(parent)
-        panel.grid(column=2, row=0)
+        panel.grid(column=2, row=0, sticky=N+S)
 
-        stat_panel = tk.Frame(panel, borderwidth=2, relief='ridge', width=500, height=550)
-        stat_panel.grid(column=0, row=0, rowspan=2)
-
-
-def _create_left_drawer(root, context: str="none") -> tk.Frame:
-    left_drawer = tk.Frame(root, borderwidth=2, relief='sunken', width=300, height=500)
-    left_drawer.grid(column=0, row=1, sticky=N+W+S+E)
-    left_drawer.grid_propagate(False)
-
-    return left_drawer
+        stat_panel = tk.Frame(panel, borderwidth=2, relief='ridge', width=500, height=573)
+        stat_panel.grid(column=0, row=0, rowspan=2, sticky=N+S)
 
 
 # declare global variables
-buttons = {}
-labels = {}
 
 # load the data
 loans = load_loans()
